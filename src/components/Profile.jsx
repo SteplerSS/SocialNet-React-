8f90f06152791150a9ce5f31133c63552b9b30f0
S.Rodionov <SNRodionovv@gmail.com>
2020-01-25 17:06:47 -0600

    #2 commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='profile'>
+            <div>
+                 <img/>
+            </div>
+            <div>
+                 ava +iscription
+            </div>
+            <div>
+                     py post
+               <div>
+                     New post
+               </div>
+               <div>
+                   <div>
+                           post1
+                   </div>
+                   <div>
+                           post2
+                   </div>
+               </div>
+               <div>
+                   Main content
+               </div>
+             </div>
+        </div>
+   )
+};
+export default Profile;
