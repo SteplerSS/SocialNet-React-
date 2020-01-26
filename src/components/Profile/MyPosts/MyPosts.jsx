1842eb60f0670763f9508c6715fcf8c166263882
S.Rodionov <SNRodionovv@gmail.com>
2020-01-25 23:46:01 -0600

    #4.1

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
 import Post from "./Post/Post";
+import s from './MyPosts.module.css'
 
 
 const MyPosts = () => {
+
     return (
             <div>
                      MY POST
@@ -14,10 +16,11 @@ const MyPosts = () => {
                <div>
                      NEW POST
                </div>
-                <Post/>
-                <Post/>
-                <Post/>
-                <Post/>
+                <div className={s.posts}>
+                    <Post message='Hi, how are you?'/>
+                    <Post message='It is my first post' />
+                </div>
+
             </div>
 
    )
