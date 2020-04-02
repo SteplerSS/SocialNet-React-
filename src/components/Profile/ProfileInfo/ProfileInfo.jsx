005bf9d3b39fc21ce8d4b5456ed3a02498f4d743
S.Rodionov <SNRodionovv@gmail.com>
2020-04-01 23:34:47 -0500

    #84_04/01/2020

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import p from './ProfileInfo.module.css'
 import Preloader from '../../common/preloader/Preloader';
-import ProfileStatus from "./ProfileStatus"
+import ProfileStatusWithHooks from "./ProfileStatusWithHooks"
 
 
 const ProfileInfo = (props) => {
@@ -17,7 +17,7 @@ const ProfileInfo = (props) => {
                 </div> */}
                 <div className={p.ava}>
                     <img src ={props.profile.photos.large}/>
-                   <ProfileStatus status={props.status} updateStatus={props.updateStatus}/>
+                   <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus}/>
                 </div>
             </div>
         </div>
